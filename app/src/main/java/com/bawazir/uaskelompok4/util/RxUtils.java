daad4639593619126258ed979ec7c0832c6e66b1
bagasbawazir <bawazirfadhil@gmail.com>
2019-05-26 15:01:24 +0700

    tambah layout

---
+++ app/src/main/java/com/bawazir/uaskelompok4/util/RxUtils.java
@@ -0,0 +1,25 @@
+package com.bawazir.uaskelompok4.util;
+
+import io.reactivex.disposables.Disposable;
+
+/**
+ * @author arun
+ */
+public class RxUtils
+{
+    public static void unsubscribe(Disposable subscription)
+    {
+        if (subscription != null && !subscription.isDisposed())
+        {
+            subscription.dispose();
+        } // else subscription doesn't exist or already unsubscribed
+    }
+
+    public static void unsubscribe(Disposable... subscriptions)
+    {
+        for (Disposable subscription : subscriptions)
+        {
+            unsubscribe(subscription);
+        }
+    }
+}
